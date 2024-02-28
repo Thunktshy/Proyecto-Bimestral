aab817dcdbd79a6bc7b7de0ff06939029d47f03f
Ricardo Flores <tunktshi@gmail.com>
2024-02-28 09:00:31 -0600

    AssemblyInfoInputs

--- src/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ src/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e124516673bb3932481395a91c5d5b711d886058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127c7e360c7d4705864488cc427675f3d956c0a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
